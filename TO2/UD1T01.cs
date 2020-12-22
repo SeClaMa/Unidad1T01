82b7f170da4abfa85320ff66bb0eea2f749b0060
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-22 20:43:23 +0100

    Agregar archivos de proyecto.

---
+++ TO2/UD1T01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TO2
+{
+    class UD1T01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!(Tarea 02 de C1)");
+            Console.ReadKey();
+        }
+    }
+}
